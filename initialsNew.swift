8ee75b95112400057518cb323a00fe37c9072625
RMB MYR <58590065+RMB-MYR@users.noreply.github.com>
2025-02-23 15:07:46 +0100

    Create initialsNew.swift

---
+++ initialsNew.swift
@@ -0,0 +1,20 @@
+// Hi, I am RMB MYR, and I am learning Swift.
+
+
+print("RRRR   M     M  BBBB    ")
+print("R   R  MM   MM  B   B   ")
+print("R   R  MM   MM  B   B   ")
+print("RRRR   M  M  M  BBBB    ")
+print("R R    M     M  B   B   ")
+print("R  R   M     M  B   B   ")
+print("R   R  M     M  BBBB    ")
+print("                      ")
+
+print("M     M  Y    Y  RRRR   ")
+print("MM   MM   Y  Y   R   R  ")
+print("MM   MM    Y     R   R  ")
+print("M  M  M    Y     RRRR   ")
+print("M     M    Y     R R    ")
+print("M     M    Y     R  R   ")
+print("M     M    Y     R   R  ")
+
